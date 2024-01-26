68c4bfef2d7ded7199125eb5742d030632ca9ff1
mitstuu <ethansmith6621@gmail.com>
2024-01-26 20:19:21 +0000

    Add new commands and functionalities

--- cafebot.py
+++ cafebot.py
@@ -17,6 +17,9 @@ client = commands.Bot(command_prefix='b!', intents=intents)
 cogs = ['user_commands']
 TOKEN = os.getenv('BARISTA_BOT_TOKEN')
 
+# Replace TOKEN with your bot's token
+TOKEN = os.environ['BARISTA_BOT_TOKEN']
+
 @client.event
 async def on_ready_event():
     await client.change_presence(activity=Game(name="with the API"))
@@ -32,69 +35,64 @@ async def on_ready_event():
 
 # Replace CHANNEL_ID with the ID of the channel where you want to display the kick message
 
-ids: dict[str, int] = {
-    'bonk_channel'      :   934288549946216541,
-    'general_channel'   :   934288549266739224,
-    'role'              :   934288548474007576,
-}
-bonk_CHANNEL_ID = 934288549946216541
-general_CHANNEL_ID = 934288549266739224
 
+# bonk_CHANNEL_ID = 934288549946216541
+# general_CHANNEL_ID = 934288549266739224
+ids: dict[str, int] = {
+        'bonk_channel'      :   934288549946216541,
+        'general_channel'   :   934288549266739224,
+        'chat_reviver'      :   934288548474007577,
+        }
 
 
 #@Welcomers role ID
 
 ROLE_ID = 934288548474007576
 
-# Replace TOKEN with your bot's token
-TOKEN = os.environ['BARISTA_BOT_TOKEN']
 
 
-@client.event
-async def on_ready():
-    await client.change_presence(activity=Game(name="with the API"))
 
 # Set the cooldown duration to 30 minutes (1800 seconds)
 #cooldown_duration = commands.Cooldown(rate=1, per=1800, type=commands.BucketType.member)
 
 #   Info commands
 
-# Define the serverinfo command
-@client.command(name='serverinfo')
-async def server_info(ctx):
-    server = ctx.guild
-    number_of_text_channels = len(server.text_channels)
-    number_of_voice_channels = len(server.voice_channels)
-    server_description = server.description
-    number_of_people = server.member_count
-    server_name = server.name
-    server_picture = server.icon.url if server.icon else None
+# # Define the serverinfo command
+# @client.command(name='serverinfo')
+# async def server_info(ctx):
+#     server = ctx.guild
+#     number_of_text_channels = len(server.text_channels)
+#     number_of_voice_channels = len(server.voice_channels)
+#     server_description = server.description
+#     number_of_people = server.member_count
+#     server_name = server.name
+#     server_picture = server.icon.url if server.icon else None
 
-    embed = discord.Embed(title=server_name, description=server_description, color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
-    if server_picture:
-        embed.set_thumbnail(url=server_picture)
-    embed.add_field(name="Members", value=number_of_people)
-    embed.add_field(name="Text Channels", value=number_of_text_channels)
-    embed.add_field(name="Voice Channels", value=number_of_voice_channels)
+#     embed = discord.Embed(title=server_name, description=server_description, color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
+#     if server_picture:
+#         embed.set_thumbnail(url=server_picture)
+#     embed.add_field(name="Members", value=number_of_people)
+#     embed.add_field(name="Text Channels", value=number_of_text_channels)
+#     embed.add_field(name="Voice Channels", value=number_of_voice_channels)
 
-    embed.set_footer(text="Sent:")
-    await ctx.send(embed=embed)
+#     embed.set_footer(text="Sent:")
+#     await ctx.send(embed=embed)
 
-# Define the userinfo command
-@client.command(name='userinfo')
-async def user_info(ctx, target: Union[discord.Member, None] = None):
-    target = target or ctx.author
-    user_roles = [role for role in target.roles]
-    embed = discord.Embed(title="User information", color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
-    embed.set_thumbnail(url=target.avatar)
-    embed.set_footer(text="Sent")
-    embed.add_field(name="ID:", value=target.id, inline=False)
-    embed.add_field(name="Name:", value=target.display_name, inline=False)
-    embed.add_field(name="Created at:", value=target.created_at.strftime("%d/%m/%Y %H:%M:%S"), inline=False)
-    if target.joined_at is not None:
-        embed.add_field(name="Joined at:", value=target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), inline=False)
-    embed.add_field(name=f"Roles ({len(user_roles)})", value=" ".join([role.mention for role in user_roles]), inline=False)
-    await ctx.send(embed=embed)
+# # Define the userinfo command
+# @client.command(name='userinfo')
+# async def user_info(ctx, target: Union[discord.Member, None] = None):
+#     target = target or ctx.author
+#     user_roles = [role for role in target.roles]
+#     embed = discord.Embed(title="User information", color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
+#     embed.set_thumbnail(url=target.avatar)
+#     embed.set_footer(text="Sent")
+#     embed.add_field(name="ID:", value=target.id, inline=False)
+#     embed.add_field(name="Name:", value=target.display_name, inline=False)
+#     embed.add_field(name="Created at:", value=target.created_at.strftime("%d/%m/%Y %H:%M:%S"), inline=False)
+#     if target.joined_at is not None:
+#         embed.add_field(name="Joined at:", value=target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), inline=False)
+#     embed.add_field(name=f"Roles ({len(user_roles)})", value=" ".join([role.mention for role in user_roles]), inline=False)
+#     await ctx.send(embed=embed)
 
     #  Listeners
 
@@ -103,7 +101,7 @@ async def user_info(ctx, target: Union[discord.Member, None] = None):
 async def on_member_join(member):
     # Get the account age of the member in minutes
     age_minutes = (datetime.now(timezone.utc) - member.created_at).total_seconds() // 60
-    gchannel = client.get_channel(general_CHANNEL_ID)
+    gchannel = client.get_channel(ids['general_channel'])
     role = member.guild.get_role(ROLE_ID)
     
     if isinstance(gchannel, discord.TextChannel):  # Check if gchannel is a TextChannel
@@ -114,38 +112,32 @@ async def on_member_join(member):
             await gchannel.send(f'Welcome {member.mention} to the server! {role.mention}s assemble!')
 
 
-# Define the ping command
+# # Define the revivechat command
 # @client.command()
-# async def ping(ctx):
-#     await ctx.send(f'Pong! Latency: `{round(client.latency * 1000)}ms`')
-
-
-# Define the revivechat command
-@client.command()
-@commands.cooldown(1, 1800, commands.BucketType.guild)  # Apply the cooldown to the command
-async def revivechat(ctx):
-    role = ctx.guild.get_role(934288548474007577)
-    await ctx.send(f'{ctx.author.mention} would like to revive this chat. {role.mention}s assemble!')
+# @commands.cooldown(1, 1800, commands.BucketType.guild)  # Apply the cooldown to the command
+# async def revivechat(ctx):
+#     role = ctx.guild.get_role(934288548474007577)
+#     await ctx.send(f'{ctx.author.mention} would like to revive this chat. {role.mention}s assemble!')
 
 # Error handling for the revivechat command cooldown
-@revivechat.error
-async def revivechat_error(ctx, error):
-    if isinstance(error, commands.CommandOnCooldown):
-        remaining = error.retry_after
-        minutes = remaining // 60
-        seconds = remaining % 60
-        await ctx.send(f"This command is on cooldown. Please try again in {minutes} minutes and {seconds} seconds.")
+# @revivechat.error
+# async def revivechat_error(ctx, error):
+#     if isinstance(error, commands.CommandOnCooldown):
+#         remaining = error.retry_after
+#         minutes = remaining // 60
+#         seconds = remaining % 60
+#         await ctx.send(f"This command is on cooldown. Please try again in {minutes} minutes and {seconds} seconds.")
 
-# Define the help command
-@client.command()
-async def commandhelp(ctx):
-    embed = discord.Embed(title="Help", description="List of available commands:", color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
-    embed.add_field(name="b!serverinfo", value="Displays information about the server")
-    embed.add_field(name="b!userinfo", value="Displays information about a user")
-    embed.add_field(name="b!ping", value="Displays the bot's current latency")
-    embed.add_field(name="b!revivechat", value="Asks the Welcomers to revive the chat")
-    embed.set_footer(text="Sent:")
-    await ctx.send(embed=embed)
+# # Define the help command
+# @client.command()
+# async def commandhelp(ctx):
+#     embed = discord.Embed(title="Help", description="List of available commands:", color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
+#     embed.add_field(name="b!serverinfo", value="Displays information about the server")
+#     embed.add_field(name="b!userinfo", value="Displays information about a user")
+#     embed.add_field(name="b!ping", value="Displays the bot's current latency")
+#     embed.add_field(name="b!revivechat", value="Asks the Welcomers to revive the chat")
+#     embed.set_footer(text="Sent:")
+#     await ctx.send(embed=embed)
 
 
 async def main():
--- user_commands.py
+++ user_commands.py
@@ -1,19 +1,97 @@
 import discord
 from discord.ext import commands
+from datetime import datetime, timezone, timedelta
+from typing import Union
+from discord import Game
 
 class UserCommands(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
+        self.ids: dict[str, int] = {
+        'bonk_channel'      :   934288549946216541,
+        'general_channel'   :   934288549266739224,
+        'chat_reviver'      :   934288548474007577,
+        }
 
     @commands.command()
     async def hello(self, ctx):
+        """ Say hello to the bot """
         await ctx.send("Hello, user!")
 
     # Define the ping command
     @commands.command()
     async def ping(self, ctx):
+        """ Get the bot's latency """
         await ctx.send(f'Pong! Latency: `{round(self.bot.latency * 1000)}ms`')
+
+    @commands.command(name='serverinfo')
+    async def server_info(self, ctx):
+        """ Get information about the server """
+        server = ctx.guild
+        number_of_text_channels = len(server.text_channels)
+        number_of_voice_channels = len(server.voice_channels)
+        server_description = server.description
+        number_of_people = server.member_count
+        server_name = server.name
+        server_picture = server.icon.url if server.icon else None
+        # Create the embed
+        embed = discord.Embed(title=server_name, description=server_description, color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
+        if server_picture:
+            embed.set_thumbnail(url=server_picture)
+        embed.add_field(name="Members", value=number_of_people)
+        embed.add_field(name="Text Channels", value=number_of_text_channels)
+        embed.add_field(name="Voice Channels", value=number_of_voice_channels)
+        embed.set_footer(text="Sent:")
+
+        await ctx.send(embed=embed)
+
+    # Define the userinfo command
+    @commands.command(name='userinfo')
+    async def user_info(self, ctx, target: Union[discord.Member, None] = None):
+        """ Get information about a user """
+        target = target or ctx.author
+        user_roles = [role for role in target.roles]
+        embed = discord.Embed(title="User information", color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
+        embed.set_thumbnail(url=target.avatar)
+        embed.set_footer(text="Sent")
+        embed.add_field(name="ID:", value=target.id, inline=False)
+        embed.add_field(name="Name:", value=target.display_name, inline=False)
+        embed.add_field(name="Created at:", value=target.created_at.strftime("%d/%m/%Y %H:%M:%S"), inline=False)
+        if target.joined_at is not None:
+            embed.add_field(name="Joined at:", value=target.joined_at.strftime("%d/%m/%Y %H:%M:%S"), inline=False)
+        embed.add_field(name=f"Roles ({len(user_roles)})", value=" ".join([role.mention for role in user_roles]), inline=False)
+    
+        await ctx.send(embed=embed)
+
+    # Define the revivechat command
+    @commands.command()
+    @commands.cooldown(1, 1800, commands.BucketType.guild)  # Apply the cooldown to the command
+    async def revivechat(self, ctx):
+        """ Revive the chat """
+        # Get the role
+        role = ctx.guild.get_role(self.ids['chat_reviver'])
+        await ctx.send(f'{ctx.author.mention} would like to revive this chat. {role.mention}s assemble!')
+
+    @commands.Cog.listener()
+    async def revivechat_error(self, ctx, error):
+        if isinstance(error, commands.CommandOnCooldown):
+            remaining = error.retry_after
+            minutes = remaining // 60
+            seconds = remaining % 60
+            await ctx.send(f"This command is on cooldown. Please try again in {minutes} minutes and {seconds} seconds.")
+
+    # Define the help command
+    @commands.command()
+    async def commandhelp(self, ctx):
+        embed = discord.Embed(title="Help", description="List of available commands:", color=discord.Color.from_rgb(126, 169, 107), timestamp=datetime.utcnow())
+        embed.add_field(name="b!serverinfo", value="Displays information about the server")
+        embed.add_field(name="b!userinfo", value="Displays information about a user")
+        embed.add_field(name="b!ping", value="Displays the bot's current latency")
+        embed.add_field(name="b!revivechat", value="Asks the Welcomers to revive the chat")
+        embed.set_footer(text="Sent:")
+        await ctx.send(embed=embed)
+
 
 
 async def setup(client):
